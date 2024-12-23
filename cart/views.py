41598d566263581d6e4ef56f8a20ad24cf776c54
peter vol <petervol35@gmail.com>
2024-12-23 18:57:00 +0200

    tried cart, stupid

--- cart/views.py
+++ cart/views.py
@@ -1,2 +1,4 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from .models import Cart, CartItem
+from .serializers import CartItemSerializer, CartSerializer
--- shop/urls.py
+++ shop/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("carts/", include("cart.urls")),
+    path("cart/", include("cart.urls")),
     path("products/", include("product.urls")),
 ]
